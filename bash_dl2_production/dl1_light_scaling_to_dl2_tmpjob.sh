b1c26b4c3a028cae4c55a93d9b6af4d8c4e6492e
juanjq <juanjimenezquiles34@gmail.com>
2023-12-14 09:33:38 +0000

    All sampling for correlations done

--- bash_dl2_production/dl1_light_scaling_to_dl2_tmpjob.sh
+++ bash_dl2_production/dl1_light_scaling_to_dl2_tmpjob.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /fefs/aswg/workspace/juan.jimenez/cosmic_ray_data_correction/bash_dl2_production/script_process_dl1a_to_dl2_run.py '0-1921-82-1.0,2.9887764107479398,2.58877641074794,3.1387764107479397'
+python /fefs/aswg/workspace/juan.jimenez/cosmic_ray_data_correction/bash_dl2_production/script_process_dl1a_to_dl2_run.py '0-1921-82-1.0,2.9887764107479398,2.58877641074794,2.6804430774146066,2.772109744081273,2.8637764107479398,2.9554430774146065,3.047109744081273,3.1387764107479397'
